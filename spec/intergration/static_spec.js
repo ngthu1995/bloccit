358a2c723a3b8748249ec79b0ded7d9ec062ae68
Thu Nguyen <ngthu1995@gmail.com>
2019-03-08 17:40:25 -0500

    finish express-intro-assignment

--- spec/intergration/static_spec.js
+++ spec/intergration/static_spec.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/";
+const baseMarco = "http://localhost:3000/marco";
 
 describe("routes : static", () => {
   //#1
@@ -10,6 +11,20 @@ describe("routes : static", () => {
       //#3
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+
+        //#4
+        done();
+      });
+    });
+  });
+
+  describe("GET /marco", () => {
+    //#2
+    it("should return status code 200 with the string 'polo'", done => {
+      //#3
+      request.get("http://localhost:3000/marco", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(res.body).toBe("polo");
 
         //#4
         done();
