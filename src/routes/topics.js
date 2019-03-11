993c224509a5b080b0577b2892bba333fdd11053
Thu Nguyen <ngthu1995@gmail.com>
2019-03-11 17:06:19 -0400

    relation issues

--- src/routes/topics.js
+++ src/routes/topics.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const topicController = require("../controllers/topicController");
 
-router.get("/topics", topicController);
+router.get("/topics", topicController.index);
 
 module.exports = router;
