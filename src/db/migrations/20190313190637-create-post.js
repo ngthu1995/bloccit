44ea29e7ffc524badfe1eff63002d0002c17d4ff
Thu Nguyen <ngthu1995@gmail.com>
2019-03-13 21:31:38 -0400

    finish post-resource-1

--- src/db/migrations/20190313190637-create-post.js
+++ src/db/migrations/20190313190637-create-post.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("Topics", {
+    return queryInterface.createTable("Posts", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -12,7 +12,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
-      description: {
+      body: {
         allowNull: false,
         type: Sequelize.STRING
       },
@@ -23,10 +23,21 @@ module.exports = {
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
+      },
+      topicId: {
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE", // delete post if parent topic is deleted
+        allowNull: false, // validation to prevent null value
+        references: {
+          // association information
+          model: "Topics", // table name
+          key: "id", // attribute to use
+          as: "topicId" // reference as topicId
+        }
       }
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable("Topics");
+    return queryInterface.dropTable("Posts");
   }
 };
