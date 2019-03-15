33a6f0ade72dbf82ca88e7a57a9fa50bb5cfadaa
Thu Nguyen <ngthu1995@gmail.com>
2019-03-14 20:51:22 -0400

    finish post-resource-2

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,4 +1,5 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
   //#1
@@ -28,7 +29,15 @@ module.exports = {
       });
   },
   getTopic(id, callback) {
-    return Topic.findById(id)
+    return Topic.findById(id, {
+      //#3
+      include: [
+        {
+          model: Post,
+          as: "posts"
+        }
+      ]
+    })
       .then(topic => {
         callback(null, topic);
       })
