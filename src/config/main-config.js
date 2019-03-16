834f86783aefec59e54a498b7e50f9c1fbdf76e5
Thu Nguyen <ngthu1995@gmail.com>
2019-03-15 22:48:41 -0400

    install faker dependency

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
 const bodyParser = require("body-parser");
+const expressValidator = require("express-validator");
 
 module.exports = {
   init(app, express) {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(expressValidator());
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
--- src/db/seeds/20190312214059-topics.js
+++ src/db/seeds/20190312214059-topics.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//#1
 const faker = require("faker");
 
 //#2
@@ -14,23 +15,30 @@ for (let i = 1; i <= 15; i++) {
   });
 }
 
+//#3
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Topics", topics, {});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
 
-    Example: return queryInterface.bulkInsert(
-      "Person",
-      [
-        {
-          name: "John Doe",
-          isBetaMember: false
-        }
-      ],
-      {}
-    );
+      Example:
+      return queryInterface.bulkInsert("Person", [{
+        name: "John Doe",
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert("Topics", topics, {});
   },
 
   down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete("Person", null, {});
+    */
     return queryInterface.bulkDelete("Topics", null, {});
   }
 };
