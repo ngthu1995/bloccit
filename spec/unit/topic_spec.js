02474d9478bee469eba93aea730109819d56752c
Thu Nguyen <ngthu1995@gmail.com>
2019-03-17 22:27:05 -0400

    work on authentication checkpoint with topics_spec fail

--- spec/unit/topic_spec.js
+++ spec/unit/topic_spec.js
@@ -1,35 +1,52 @@
 const sequelize = require("../../src/db/models/index").sequelize;
 const Topic = require("../../src/db/models").Topic;
 const Post = require("../../src/db/models").Post;
+const User = require("../../src/db/models").User;
 
 describe("Post", () => {
   beforeEach(done => {
     //#1
     this.topic;
     this.post;
+    this.user;
+
     sequelize.sync({ force: true }).then(res => {
-      //#2
-      Topic.create({
-        title: "Adventure to Africa",
-        description: "Unknown lands with friendly people"
-      })
-        .then(topic => {
-          this.topic = topic;
-          //#3
-          Post.create({
-            title: "The very first visit",
-            body: "I saw some rocks.",
-            //#4
-            topicId: this.topic.id
-          }).then(post => {
-            this.post = post;
-            done();
-          });
-        })
-        .catch(err => {
-          console.log(err);
+      // #2
+      User.create({
+        email: "starman@tesla.com",
+        password: "Trekkie4lyfe"
+      }).then(user => {
+        this.user = user; //store the user
+
+        // #3
+        Topic.create(
+          {
+            title: "Expeditions to Alpha Centauri",
+            description:
+              "A compilation of reports from recent visits to the star system.",
+
+            // #4
+            posts: [
+              {
+                title: "My first visit to Proxima Centauri b",
+                body: "I saw some rocks.",
+                userId: this.user.id
+              }
+            ]
+          },
+          {
+            // #5
+            include: {
+              model: Post,
+              as: "posts"
+            }
+          }
+        ).then(topic => {
+          this.topic = topic; //store the topic
+          this.post = topic.posts[0]; //store the post
           done();
         });
+      });
     });
   });
 
@@ -68,7 +85,9 @@ describe("Post", () => {
   describe("#getPosts()", () => {
     it("should return the associated post", done => {
       this.topic.getPosts().then(associatedPost => {
-        expect(associatedPost[0].title).toBe("The very first visit");
+        expect(associatedPost[0].title).toBe(
+          "My first visit to Proxima Centauri b"
+        );
         done();
       });
     });
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const postController = require("../controllers/postController");
 const validation = require("./validation");
+const helper = require("../auth/helpers");
 
 router.get("/topics/:topicId/posts/new", postController.new);
 
 router.post(
   "/topics/:topicId/posts/create",
+  helper.ensureAuthenticated,
   validation.validatePosts,
   postController.create
 );
