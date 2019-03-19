5b1231c53b5226813aea0ff47be7415cfaea763c
Thu Nguyen <ngthu1995@gmail.com>
2019-03-19 01:09:09 -0400

    create vote model

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -35,6 +35,10 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "comments"
     });
+    Post.hasMany(models.Vote, {
+      foreignKey: "postId",
+      as: "votes"
+    });
   };
   return Post;
 };
