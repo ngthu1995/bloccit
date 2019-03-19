4062fbc3b21091a9fd7ec69bb4cdd6d92cf635b4
Thu Nguyen <ngthu1995@gmail.com>
2019-03-19 15:19:52 -0400

    finish voting checkpoint

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -39,6 +39,19 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "votes"
     });
+    Post.prototype.getPoints = function() {
+      // #1
+      if (this.votes.length === 0) return 0;
+
+      // #2
+      return this.votes
+        .map(v => {
+          return v.value;
+        })
+        .reduce((prev, next) => {
+          return prev + next;
+        });
+    };
   };
   return Post;
 };
